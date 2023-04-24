540e0388498b138388ab8a5ad7a4d4517541834e
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2023-04-24 11:48:18 +0200

    cleaned triangle

--- source/Triangle.h
+++ source/Triangle.h
@@ -7,17 +7,18 @@ class Triangle : public Geometry
 {
 public:
 	Triangle(const Elite::FPoint3& pos, const std::vector<Vertex_Input>& localVertices = {});
-	virtual ~Triangle() = default;
+	~Triangle() override = default;
 
 	Triangle(const Triangle&) = delete;
 	Triangle(Triangle&&) noexcept = delete;
 	Triangle& operator=(const Triangle&) = delete;
 	Triangle& operator=(Triangle&&) noexcept = delete;
 
-	virtual std::vector<Elite::FPoint4> GetVerticesWorldPositions() override final;
-	virtual const std::vector<Vertex_Input>& GetColoredVertices() const final;
+	std::vector<Elite::FPoint4> GetVerticesWorldPositions() override final;
+	[[nodiscard]] const std::vector<Vertex_Input>& GetColoredVertices() const;
 
 	void SetLocalVertices(const std::vector<Vertex_Input>& newLocalVertices);
+
 protected:
 	std::vector<Vertex_Input> m_TriangleVertices;
 };
