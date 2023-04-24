2f862785c335f7beb76e7c42ca660b804678e187
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2023-04-24 11:58:10 +0200

    rule of 5 texture

--- source/Texture.h
+++ source/Texture.h
@@ -1,15 +1,20 @@
 #pragma once
 
-class Texture
+class Texture final
 {
 public:
 	Texture(const std::string& filePath, ID3D11Device* pDevice);
 	~Texture();
 
+	Texture(const Texture& other) noexcept = delete;
+	Texture(Texture&& other) noexcept = delete;
+	Texture& operator=(const Texture& other) noexcept = delete;
+	Texture& operator=(Texture&& other) noexcept = delete;
+
 	[[nodiscard]] ID3D11ShaderResourceView* GetResourceView() const;
 	[[nodiscard]] ID3D11Texture2D* GetTexture() const;
 
-	Elite::RGBColor Sample(const Elite::FVector2& uv) const;
+	[[nodiscard]] Elite::RGBColor Sample(const Elite::FVector2& uv) const;
 	[[nodiscard]] Uint32 PixelToIndex(const Elite::FVector2& uv) const;
 
 private:
